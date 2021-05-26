827600ee8e43b560b65153437afd6aa4edde892b
matthew_hall <matthew.hall1@my.jcu.edu.au>
2021-05-26 11:47:57 +1000

    Modify margins and footer in jazz_theme

--- jazzClubSiteFiles/wp-content/themes/jazz_theme/footer.php
+++ jazzClubSiteFiles/wp-content/themes/jazz_theme/footer.php
@@ -13,16 +13,10 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'jazz_theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'jazz_theme' ), 'WordPress' );
-				?>
-			</a>
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'jazz_theme' ), 'jazz_theme', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'jazz_theme' ), 'jazz_theme', '<a href="https://github.com/cp3402-students/cp3402-2021-site-cp3402-2021-team15">Team 15</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
